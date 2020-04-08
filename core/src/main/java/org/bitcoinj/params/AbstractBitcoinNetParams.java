e5818c5f367814ef0a70cc8afdef7fd1fd2d6de0
pokkst <pokkst@protonmail.com>
2020-04-08 14:09:52 -0500

    Add ComputeTarget method

--- core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
+++ core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
@@ -22,6 +22,7 @@ import static com.google.common.base.Preconditions.checkState;
 import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.base.Preconditions;
 import org.bitcoinj.core.*;
 import org.bitcoinj.utils.MonetaryFormat;
 import org.bitcoinj.store.BlockStore;
@@ -83,6 +84,43 @@ public abstract class AbstractBitcoinNetParams extends NetworkParameters {
         } catch (BlockStoreException e) {
             throw new RuntimeException("Cannot determine monolith activation without BlockStore");
         }
+    }
+
+    /**
+     * Compute the a target based on the work done between 2 blocks and the time
+     * required to produce that work.
+     */
+    public static BigInteger ComputeTarget(StoredBlock pindexFirst,
+                                           StoredBlock pindexLast) {
+
+        Preconditions.checkState(pindexLast.getHeight() > pindexFirst.getHeight());
+
+        /*
+         * From the total work done and the time it took to produce that much work,
+         * we can deduce how much work we expect to be produced in the targeted time
+         * between blocks.
+         */
+        BigInteger work = pindexLast.getChainWork().subtract(pindexFirst.getChainWork());
+        work = work.multiply(BigInteger.valueOf(TARGET_SPACING));
+
+        // In order to avoid difficulty cliffs, we bound the amplitude of the
+        // adjustement we are going to do.
+        //assert(pindexLast->nTime > pindexFirst->nTime);
+        long nActualTimespan = pindexLast.getHeader().getTimeSeconds() - pindexFirst.getHeader().getTimeSeconds();
+        if (nActualTimespan > 288 * TARGET_SPACING) {
+            nActualTimespan = 288 * TARGET_SPACING;
+        } else if (nActualTimespan < 72 * TARGET_SPACING) {
+            nActualTimespan = 72 * TARGET_SPACING;
+        }
+
+        work = work.divide(BigInteger.valueOf(nActualTimespan));
+
+        /*
+         * We need to compute T = (2^256 / W) - 1 but 2^256 doesn't fit in 256 bits.
+         * By expressing 1 as W / W, we get (2^256 - W) / W, and we can compute
+         * 2^256 - W as the complement of W.
+         */
+        return LARGEST_HASH.divide(work).subtract(BigInteger.ONE);//target.add(BigInteger.ONE))
     }
 
     /**
