25b865769ce779b082687b69698216cc89fff1cf
pokkst <pokkst@protonmail.com>
2020-06-17 06:12:42 -0500

    Remove unused code

--- core/src/test/java/org/bitcoinj/core/AddressConversionTests.java
+++ core/src/test/java/org/bitcoinj/core/AddressConversionTests.java
@@ -24,25 +24,5 @@ public class AddressConversionTests {
 
         System.out.println(CashAddress.fromCashAddress(params, "bitcoincash:qrnpf0nqkzgxpvjzqlqcqm4h78sg45zyu54rngdxff").getOutputScriptType());
         System.out.println(CashAddress.fromCashAddress(params, "bitcoincash:pqeft5ukkg9ew2fgt5axxnrq853nc5cuyvvpusv2nz").getOutputScriptType());
-        /*String cashAddress = "qzhr268ppgwtr36h20apc6ahzx6lsmlmcy40qejee0";
-        assertTrue(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2PKHNoPrefixValid));
-
-        String cashAddrP2SHValid = "bitcoincash:pqeft5ukkg9ew2fgt5axxnrq853nc5cuyvvpusv2nz";
-        assertTrue(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2SHValid));
-
-        String cashAddrP2SHNoPrefixValid = "pqeft5ukkg9ew2fgt5axxnrq853nc5cuyvvpusv2nz";
-        assertTrue(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2SHNoPrefixValid));
-
-        String cashAddrP2PKHInvalid = "bitcoincash:qzhr268ppgwtr36h2smlmcy40qejeec";
-        assertFalse(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2PKHInvalid));
-
-        String cashAddrP2PKHNoPrefixInvalid = "qzhr268ppgwtr36xclsmlmcy40qejeec";
-        assertFalse(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2PKHNoPrefixInvalid));
-
-        String cashAddrP2SHInvalid = "bitcoincash:pqef5axxnrq853nc5cuyvvpusv2nz";
-        assertFalse(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2SHInvalid));
-
-        String cashAddrP2SHNoPrefixInvalid = "pqeft5ukkg9ew2fgt5axxncuyvvpusv2nz";
-        assertFalse(Address.isValidCashAddr(MainNetParams.get(), cashAddrP2SHNoPrefixInvalid));*/
     }
 }
