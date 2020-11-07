c307ab0d511e2099fbd45683be777f11854153e1
pokkst <pokkst@protonmail.com>
2020-11-07 15:37:57 -0600

    Revert "Base off tx outpoint"
    This reverts commit 40ea7855

--- core/src/main/java/org/bitcoinj/core/MultisigInput.java
+++ core/src/main/java/org/bitcoinj/core/MultisigInput.java
@@ -5,6 +5,6 @@ import org.bitcoinj.crypto.MultisigSignature;
 import java.util.ArrayList;
 
 public class MultisigInput {
-    public String outpoint;
+    public String txid;
     public ArrayList<MultisigSignature> signatures = new ArrayList<>();
 }
