715f8d86ca8a4ac5e1c6b0fe2717af73549b7ef3
pokkst <pokkst@protonmail.com>
2020-11-07 02:43:15 -0600

    Change MultisigPayload for experimenting

--- core/src/main/java/org/bitcoinj/utils/MultisigPayload.java
+++ core/src/main/java/org/bitcoinj/utils/MultisigPayload.java
@@ -1,11 +1,7 @@
 package org.bitcoinj.utils;
 
-import org.bitcoinj.core.MultisigInput;
-
-import java.util.ArrayList;
-
 public class MultisigPayload {
     public String address;
     public String amount;
-    public ArrayList<MultisigInput> inputs;
+    public String rawTx;
 }
