7f28278ee7f9472bb89325cc2eb499408e7cd225
pokkst <pokkst@protonmail.com>
2020-11-07 02:57:09 -0600

    Revert change

--- core/src/main/java/org/bitcoinj/utils/MultisigPayload.java
+++ core/src/main/java/org/bitcoinj/utils/MultisigPayload.java
@@ -1,7 +1,11 @@
 package org.bitcoinj.utils;
 
+import org.bitcoinj.core.MultisigInput;
+
+import java.util.ArrayList;
+
 public class MultisigPayload {
     public String address;
     public String amount;
-    public String rawTx;
+    public ArrayList<MultisigInput> inputs;
 }
